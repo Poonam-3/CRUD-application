719e39d89c7ee4d834fb2bafd5b4d197ad14d8f3
Poonam Khore <poonam.khore.pk@gmail.com>
2022-04-25 19:02:36 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+
+
+// Javascript for adding user
+$("#add_user").submit(function(event){
+    alert("Data inserted Successfully");
+})
+
+
+// Javascript for updating user
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $("#update_user").serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n,i){
+        data[n['name']] = n['value']
+
+    })
+    console.log(data);
+
+    var request = {
+        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Data Updated Successfully!");
+    })
+
+})
+
+// Javascript for deleting user
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/users/${id}`,
+            "method" : "DELETE",
+            
+        }
+
+        if(confirm("Do you want to really delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        
+
+        }
+
+
+    })
+}
